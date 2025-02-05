98e5d4a690f271d96a98a4b38d8de3c4acbd6ccc
ravena04 <153312567+ravena04@users.noreply.github.com>
2025-02-05 22:52:27 +0530

    Update LoginPage.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -12,7 +12,7 @@ export default function LoginPage() {
   async function login(ev) {
     ev.preventDefault();
     try {
-      const response = await fetch("http://localhost:4000/login", {
+      const response = await fetch("https://blogapp-backend-ttjx.onrender.com/login", {
         method: "POST",
         body: JSON.stringify({ username, password }),
         headers: { "Content-Type": "application/json" },
