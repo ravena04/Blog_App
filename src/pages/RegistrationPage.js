06a854b00534250a40e02e3bdfd431138ec6c251
ravena04 <153312567+ravena04@users.noreply.github.com>
2025-02-05 22:53:43 +0530

    Update RegistrationPage.js

--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -9,7 +9,7 @@ export default function RegisterPage() {
 
   async function register(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/register', {
+    const response = await fetch('https://blogapp-backend-ttjx.onrender.com/register', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-Type': 'application/json' },
